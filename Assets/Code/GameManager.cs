9e4e523f23724be082be25fa631c085688f8350a
Efe <efekaraman159@gmail.com>
2024-12-11 03:10:53 +0300

    AA build

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -4,44 +4,54 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public static GameManager Instance { get; private set; }
+    public static GameManager Instance { get; private set; }
 
+    [SerializeField] private ColorManager _colorManager;
 
-	[SerializeField] private ColorManager _colorManager;
+    private int score;
 
-	private void Awake()
-	{
-		Instance = this;
-	}
+    private void Awake()
+    {
+        Instance = this;
+    }
 
-	private void Start()
-	{
-		OnStart();
-	}
+    private void Start()
+    {
+        OnStart();
+    }
 
-	public void OnStart()
-	{
-		LevelManager.Instance.OnStartLevel();
-		_colorManager.SetNormal();
-	}
+    public void OnStart()
+    {
+        LevelManager.Instance.OnStartLevel();
+        _colorManager.SetNormal();
+    }
 
-	public void OnGameOver()
-	{
-		Debug.Log("Game Over");
-		Time.timeScale = 0;
-		_colorManager.OnFail();
-	}
+    public void OnGameOver()
+    {
+        Debug.Log("Game Over");
+        Time.timeScale = 0;
+        _colorManager.OnFail();
+    }
 
-	public void OnSuccess()
-	{
-		_colorManager.OnSuccess();
+    public void OnSuccess()
+    {
+        _colorManager.OnSuccess();
 
-		IEnumerator Do()
-		{
-			yield return new WaitForSeconds(1);
-			OnStart();
-		}
+        // Skoru artır
+        AddScore(1); // Her level geçişinde 1 puan ekle
 
-		StartCoroutine(Do());
-	}
+        IEnumerator Do()
+        {
+            yield return new WaitForSeconds(1);
+            OnStart();
+        }
+
+        StartCoroutine(Do());
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        Debug.Log("Score: " + score); // Skoru konsola yazdır
+    }
 }
--- Assets/Code/LevelManager.cs
+++ Assets/Code/LevelManager.cs
@@ -5,34 +5,44 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-	public static LevelManager Instance { get; private set; }
+    public static LevelManager Instance { get; private set; }
 
-	[SerializeField] private List<LevelController> levelPrefabs;
+    [SerializeField] private List<LevelController> levelPrefabs;
 
-	private int _index;
-	private LevelController _current;
+    private int _index;
+    private LevelController _current;
 
-	private void Awake()
-	{
-		Instance = this;
-	}
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-	public void OnStartLevel()
-	{
-		var prefab = levelPrefabs[_index];
-		_current = Instantiate(prefab);
-		_current.MyStart(_index);
-	}
+    public void OnStartLevel()
+    {
+        var prefab = levelPrefabs[_index];
+        _current = Instantiate(prefab);
+        _current.MyStart(_index);
+    }
 
-	public void OnFinishLevel()
-	{
-		_index++;
-		Destroy(_current.gameObject);
-		GameManager.Instance.OnSuccess();
-	}
+    public void OnFinishLevel()
+    {
+        _index++;
+        Destroy(_current.gameObject);
+        GameManager.Instance.OnSuccess();
+        GameManager.Instance.AddScore(1);  // Skor artırma
+    }
 
-	public bool IsLevelCompleted()
-	{
-		return BulletManager.Instance.bulletRemain == 0;
-	}
+    public bool IsLevelCompleted()
+    {
+        return BulletManager.Instance.bulletRemain == 0;
+    }
+
+
 }
